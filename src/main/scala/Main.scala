cf9c89a0917e5723543d9d1efee7e54925d8a9d8
Ahmed Adnan <ahmed.zangana@yahoo.com>
2023-10-20 08:25:19 +0200

    Modified Main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,28 @@
-@main def hello: Unit =
-  println("Hello world!")
-  println(msg)
+object SnakesAndLaddersGame {
+  def main(args: Array[String]): Unit = {
+    //10x10 field
+    val boardSize = 6
+    val board = createGameBoard(boardSize)
+    displayGameBoard(board)
+  }
 
-def msg = "I was compiled by Scala 3. :)"
+  def createGameBoard(size: Int): Array[Array[Int]] = {
+    val board = Array.ofDim[Int](size, size)
+    for {
+      row <- 0 until size
+      col <- 0 until size
+    } {
+      board(row)(col) = row * size + col + 1
+    }
+    board
+  }
+
+  def displayGameBoard(board: Array[Array[Int]]): Unit = {
+    for (row <- board.indices) {
+      for (col <- board(row).indices) {
+        print(f"${board(row)(col)}%3d ")
+      }
+      println()
+    }
+  }
+}
