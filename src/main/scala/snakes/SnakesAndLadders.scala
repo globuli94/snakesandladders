785f62fdf8d6a3b88084e03097b573cdf9098091
globuli94 <gallusjonathan@gmail.com>
2023-11-14 20:52:34 +0100

    test coveralls

--- src/main/scala/snakes/SnakesAndLadders.scala
+++ src/main/scala/snakes/SnakesAndLadders.scala
@@ -13,7 +13,7 @@ object SnakesAndLadders {
   controller.notifyObservers
   
   def main(args: Array[String]): Unit = {
-    println("Welcome to Snakes and Ladders!")
+    println("Welcome to Snakes and Ladders")
     var input: String = ""
 
     while(input != "exit") {
