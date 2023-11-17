67896900d7e605d45c44c400097c1dc2d39ff32c
Ahmed Adnan <ahmed.zangana@yahoo.com>
2023-11-17 01:31:25 +0100

    Random Seed Generation

--- src/main/scala/snakes/model/Board.scala
+++ src/main/scala/snakes/model/Board.scala
@@ -2,9 +2,11 @@ package snakes.model
 
 import scala.util.Random
 
-case class Board(size:Int, snakes: Map[Int, Int], ladders: Map[Int, Int])
+case class Board(size: Int, snakes: Map[Int, Int], ladders: Map[Int, Int])
 
 object Board {
+  private val random = new Random(0)
+
   def apply(size: Int): Board = {
     val (snakes, ladders) = generateSnakesAndLadders(size)
     new Board(size, snakes, ladders)
@@ -12,20 +14,24 @@ object Board {
 
   private def generateSnakesAndLadders(size: Int): (Map[Int, Int], Map[Int, Int]) = {
     val numFeatures = (size * 0.1).toInt
-    val lengths = (0.05 * size).toInt to (0.15 * size).toInt
 
-    def generateFeature(isSnake: Boolean): Map[Int, Int] = {
+    def generateFeature(isSnake: Boolean, existingFeatures: Map[Int, Int]): Map[Int, Int] = {
       var features = Map[Int, Int]()
       while (features.size < numFeatures) {
-        val start = Random.nextInt(size - 3) + 1 // avoid last 3 cells for snakes
-        val end = if (isSnake) start - Random.shuffle(lengths).head else start + Random.shuffle(lengths).head
-        if (end > 0 && end <= size && !features.contains(start) && !features.contains(end)) {
+        val start = random.nextInt(size - 1) + 1
+        val length = random.nextInt((0.25 * size).toInt) + (0.05 * size).toInt
+        val end = if (isSnake) start - length else start + length
+
+        if (end > 0 && end <= size && !features.contains(start) && !existingFeatures.contains(end) && !existingFeatures.contains(start)) {
           features += (start -> end)
         }
       }
       features
     }
 
-    (generateFeature(isSnake = true), generateFeature(isSnake = false))
+    val snakes = generateFeature(isSnake = true, Map.empty)
+    val ladders = generateFeature(isSnake = false, snakes)
+
+    (snakes, ladders)
   }
 }
