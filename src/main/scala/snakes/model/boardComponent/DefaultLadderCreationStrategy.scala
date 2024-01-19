fe6d51f3f91dcca151cd1d6f54a8480ec5209a0f
globuli94 <gallusjonathan@gmail.com>
2024-01-19 10:08:44 +0100

    modified feature generation

--- src/main/scala/snakes/model/boardComponent/DefaultLadderCreationStrategy.scala
+++ src/main/scala/snakes/model/boardComponent/DefaultLadderCreationStrategy.scala
@@ -6,7 +6,7 @@ class DefaultLadderCreationStrategy extends FeatureGenerationStrategyInterface {
   private val random = new Random(0)
 
   override def createFeatures(size: Int): Map[Int, Int] = {
-    val numFeatures = (size * 0.1).toInt
+    val numFeatures = (size * 0.07).toInt
     var features = Map[Int, Int]()
 
     while (features.size < numFeatures) {
@@ -14,7 +14,7 @@ class DefaultLadderCreationStrategy extends FeatureGenerationStrategyInterface {
       val length = random.nextInt((0.55 * size).toInt) + (0.13 * size).toInt
       val end = start + length
 
-      if (end <= size && !features.contains(start) && !features.contains(end)) {
+      if (end < size - 3 && start > 3  && !features.contains(start) && !features.contains(end)) {
         features += (start -> end)
       }
     }
--- src/main/scala/snakes/model/boardComponent/DefaultSnakeCreationStrategy.scala
+++ src/main/scala/snakes/model/boardComponent/DefaultSnakeCreationStrategy.scala
@@ -3,10 +3,10 @@ package snakes.model.boardComponent
 import scala.util.Random
 
 class DefaultSnakeCreationStrategy extends FeatureGenerationStrategyInterface {
-  private val random = new Random(0)
+  private val random = new Random(7)
 
   override def createFeatures(size: Int): Map[Int, Int] = {
-    val numFeatures = (size * 0.1).toInt
+    val numFeatures = (size * 0.07).toInt
     var features = Map[Int, Int]()
 
     while (features.size < numFeatures) {
@@ -14,7 +14,7 @@ class DefaultSnakeCreationStrategy extends FeatureGenerationStrategyInterface {
       val length = random.nextInt((0.55 * size).toInt) + (0.13 * size).toInt
       val end = start - length
 
-      if (end > 0 && !features.contains(start) && !features.contains(end)) {
+      if (end > 3 && end < size-3 && !features.contains(start) && !features.contains(end)) {
         features += (start -> end)
       }
     }
